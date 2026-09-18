c9851c18bcfeb8b3e550760a27dfc062d9704616
Атыңыз Жөнүңүз <email@example.com>
2026-09-18 15:57:04 +0600

    first commit

---
+++ lesson1/index.js
@@ -0,0 +1,23 @@
+// alert("hello world");
+// confirm("hello world");
+// prompt("what is your name?");
+
+// console.log(hello world);
+// console.log(5+5);
+// console.log(5-5);
+// console.log(5*5);
+// console.log(5/5);
+// console.log(5>5);
+
+
+
+// let - изменяемая переменная
+// const - неизменяемая переменная
+// var - старый способ объявления переменной
+
+// let name = 876419469
+// alert(name);
+
+// let name = prompt("what is your name?");
+// alert(name)
+
